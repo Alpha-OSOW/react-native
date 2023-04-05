62f5634d8ae0d92fc4841ab8631dbe4d635bd8d6
Alpha S <alphaoumar210@yahoo.fr>
2023-04-05 10:35:03 +0200

    fait

--- jour3-composants/App.js
+++ jour3-composants/App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.zone1}> 
+          <Text style={{fontSize : 30}}>premier</Text>
+          <Text>deuxieme</Text>
+    </View>
+
+          <View style={{backgroundColor : "blue" , flex : 2 , width : "100%" }}/>
+          <View style={{backgroundColor : "yellow" , flex : 0.5 , width : "100%" }}/>
+          {/*<StatusBar hidden={true} />*/}
     </View>
   );
 }
@@ -13,8 +19,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop : 25,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    },
+
+  zone1: {
+    backgroundColor : "red",
+    flex : 1 ,
+    width : "100%", 
+    flexDirection : "row",
+    justifyContent : "space-around" ,  /* attention justifyContent centerer horizontalement SI flexDirection: "row" par défaut  justifyContent: "center" va centrer Verticalement
+    flex-start
+    flex-end
+    center 
+    space-evenly
+    space-between
+    space-around
+  */ 
+    alignItems: "baseline" // verticalement SI flexDirection: "row" ,  par contre si on est sur flexDirection: "column" permet de gérer l'espacement horizontalement 
+    // flex-start
+    // flex-end
+    // center
+    // baseline
+  }
+  });
