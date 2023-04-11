3e9d714db360af2e08dd961c493a49f028bbc3c8
Alpha S <alphaoumar210@yahoo.fr>
2023-04-11 17:02:19 +0200

    fin de journée

--- jour6-menu/App.js
+++ jour6-menu/App.js
@@ -7,16 +7,24 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Accueil from "./composants/Accueil"
 import ProfilNavigation from "./composants/ProfilNavigation"
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import ConnexionNavigation from './composants/ConnexionNavigation';
 const Menu = createBottomTabNavigator()
 // rdv 13h40 
 export default function App() {
+
+  const profil = {
+    nom : "Alain" ,
+    isConnected : true   
+  }
+
+
   return (
     <View style={styles.container}>
       <NavigationContainer>
       <Menu.Navigator screenOptions={{
         tabBarActiveBackgroundColor : "#eee",
         tabBarStyle : { borderColor : "red" , borderWidth : 3 },
-        tabBarShowLabel : false, 
+        tabBarShowLabel : false
        /*  title : ((text) => { return <View>
           <Text>{JSON.stringify(text)}</Text>
           <MaterialCommunityIcons name="air-horn" color="black" size={40} />
@@ -40,6 +48,25 @@ export default function App() {
             return <MaterialCommunityIcons name="anchor" color="black" size={40} />
           }
         }}/>
+
+        { profil.isConnected 
+          ?
+            <Menu.Screen name="connexion-menu" component={ ConnexionNavigation } options={{
+              tabBarIcon : function(){
+                return <MaterialCommunityIcons name="face-man-profile" color="black" size={40} />
+              },
+              title : `Bienvenu ${profil.nom}`
+            }}/>
+          :
+          <Menu.Screen name="connexion-menu" component={ ConnexionNavigation } options={{
+            tabBarIcon : function(){
+              return <MaterialCommunityIcons name="lock-open" color="black" size={40} />
+            },
+            title : "gestion de votre profil"
+           
+          }}/>
+      }
+         
          
        </Menu.Navigator>
 
