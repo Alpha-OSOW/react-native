167516081fba19ac0fe3d6c77aed2a217d40bcd0
Alpha S <alphaoumar210@yahoo.fr>
2023-04-13 16:46:04 +0200

    store

---
+++ jour8-redux/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+function reducer (state = 200 , action){
+    switch(action.type){
+        case "AUGMENTER" :
+           return state + 1 ;
+         default :
+           return state ;
+    }
+}
+
+export const store = configureStore({reducer}) // création de store en état global
+
